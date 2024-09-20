7fca49096eb3a88c5a83dcb67a5cd148e7e576f3
Dimitri Teboul <dimitri.teboul@indy.fr>
2024-09-20 15:36:27 +0200

    ✨ feat(style): add tailwind and daisyui for styling

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,12 +1,15 @@
 use leptos::*;
+use leptos_meta::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
+        <Stylesheet id="leptos" href="/style/output.css"/>
         <button
-            class:red=move || count % 2 == 1
+            class=("text-secondary", move || count.get() % 2 == 1)
+            class="btn btn-primary m-4"
             on:click=move |_| { set_count.update(|n| *n += 1 ); }
         >
             "Click me:" {count}
